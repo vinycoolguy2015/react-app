c9fafe2c1969a7f3bda258c5f89cfa63a62bf28d
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2021-06-13 18:21:17 +0800

    fix:fixed release config

--- release.config.js
+++ release.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-	branches: "main",
-	repositoryUrl: "https://github.com/vinycoolguy2015/react-app",
-	plugins: [
-		"@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", 
-		"@semantic-release/github"
-		]
-}
+  branches: "main",
+  repositoryUrl: "https://github.com/vinycoolguy2015/react-app",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"
+  ]
+};
