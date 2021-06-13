f2c38644fbf95d3d5b2b9a1441707d164b17de71
Vinayak <vinayak.pandey@cognizant.com>
2021-06-13 18:14:12 +0800

    fix:fixed branch name

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	branches: "master",
+	branches: "main",
 	repositoryUrl: "https://github.com/vinycoolguy2015/react-app",
 	plugins: [
 		"@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", 
