4b2d40f4513c844fb5f7522b8dae58bb2fb9d98e
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2021-06-13 18:56:40 +0800

    fix: added artifacts in release

--- release.config.js
+++ release.config.js
@@ -1,9 +1,17 @@
 module.exports = {
-  branches: "main",
-  repositoryUrl: "https://github.com/vinycoolguy2015/react-app",
+  branches: "master",
+  repositoryUrl: "https://github.com/alialaa/react-app",
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/github"
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          { path: "build.zip", label: "Build" },
+          { path: "coverage.zip", label: "Coverage" }
+        ]
+      }
+    ]
   ]
 };
